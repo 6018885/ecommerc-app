942205abd5d9f13116bf9ee4de8a822eae22dd0a
oday <oday.kamal.198@gmail.com>
2022-08-09 18:00:36 +0300

    init GP

---
+++ lib/core/constant/imageasset.dart
@@ -0,0 +1,8 @@
+class AppImageAsset {
+  static const String rootImages = "assets/images";
+
+  static const String onBoardingImageOne = "$rootImages/onboardingone.PNG";
+  static const String onBoardingImageTwo = "$rootImages/onboardingtwo.PNG";
+  static const String onBoardingImagethree = "$rootImages/onboardingthree.PNG";
+  static const String onBoardingImageFuor = "$rootImages/onboardingfour.PNG";
+}
